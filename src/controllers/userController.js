b089b638ad3d979e72efcf95e0fb81840568f119
FranBag <fbagneres77@gmail.com>
2024-11-17 14:16:06 -0300

    Agregados métodos de usercontroller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,53 @@
-import { getAll } from "../models/userModel.js";
+import { createOne, deleteOne, getAll, getOneByID, updateOne } from "../models/userModel.js";
 
-export const get_all = async(req, res)=>{
-        const personas = await getAll();
-        res.json(personas);
+
+// AGREGAR HTTP CODES 200, 404, 500, etc.
+
+export const get_all_users = async(req, res) => {
+	try {
+		const users = await getAll();
+		res.json(users);
+	} catch (error) {
+		res.json(error);
+	}
 }
 
+export const get_user = async(req, res) => {
+	try {
+		const id_user = req.params.id;
+		const user = await getOneByID(id_user);
+		res.json(user);   
+	} catch (error) {
+		res.json(error);
+	}
+}
+
+export const create_user = async(req, res) => {
+	try {
+		const new_user_data = req.body.data;
+		const result = await createOne(new_user_data);
+		res.json(result);
+	} catch (error) {
+		res.json(error);
+	}
+}
+
+export const update_user = async(req, res) => {
+	try {
+		const update_user_data = req.body.data;
+		const result = await updateOne(update_user_data);
+		res.json(result);
+	} catch (error) {
+		res.json(error);
+	}
+}
+
+export const delete_user = async(req, res) => {
+	try {
+		const id_user = req.params.id;
+		const result = await deleteOne(id_user);
+		res.json(user);   
+	} catch (error) {
+		res.json(error);
+	}
+}
